0b03f42ad44a8f616901a8dad7911872d6e9562d
icrto <up201403560@fe.up.pt>
2020-08-27 00:50:24 +0100

    minor updates

--- Synthetic Dataset/example.py
+++ Synthetic Dataset/example.py
@@ -1,30 +1,19 @@
-from SyntDataset import dataset
-import numpy as np
-#example 1 - creating dataset
-#created_dataset.create_default_config_file()
+from SyntDataset import Dataset
 
-#created_dataset = dataset(config_file='config.json')
-#created_dataset = dataset()
-#if(created_dataset.create_dataset() == -1):
-    #print('Error creating dataset')
-
-#else:
-
-#example 2 - importing dataset
-imported_dataset = dataset(config_file='config.json')
-dtset = imported_dataset.import_dataset()
-print(len(dtset))
-if(dtset is not None):
-    imported_dataset.save("data_simplified.txt", dtset)
-    data = imported_dataset.load("data_simplified.txt")
-    print(len(data))
-    y = [1 if int(datum['exists']) > 0 else 0 for datum in data]
-    print(y.count(1))
-    print(y.count(0))
-    # print(dtset[1])
-    # bndboxes = imported_dataset.get_bndboxes(dtset[1])
-    # imported_dataset.display_img(dtset[1]['img'], bndboxes)
-#else:
-   # print('Error loading dataset.')
-
+# creating dataset
+dataset = Dataset(config_file="config.json")
+if dataset.create_dataset() != -1:
+    # saving & importing dataset
+    dtset = dataset.import_dataset()
+    if dtset is not None:
+        dataset.save("data_simplified.txt", dtset)
+        data = dataset.load("data_simplified.txt")
+        y = [1 if int(datum["exists"]) > 0 else 0 for datum in data]
+        print("Positive class: %d images" % y.count(1))
+        print("Negative class: %d images" % y.count(0))
+        pos_class = [datum for datum in data if int(datum["exists"]) > 0]
+        bndboxes = dataset.get_bndboxes(pos_class[1])
+        dataset.display_img(pos_class[1]["img"], bndboxes)
+    else:
+        print("Error loading dataset.")
 
